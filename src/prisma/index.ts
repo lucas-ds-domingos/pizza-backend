963f9461e662c264d9a567580193dacff58de9d6
lucas-ds-domingos <lucasdomingos.ds@hotmail.com>
2025-04-22 11:35:54 -0300

    atualizando pack5

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,6 +1,6 @@
 // src/prisma/index.ts
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prismaClient = new PrismaClient();
 
-export default prisma;
+export default prismaClient;
